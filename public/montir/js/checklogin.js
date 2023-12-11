270b52116547454e38a739dc05782e354152b71c
Nathanael Gilberth Tjandra <nathanaelgilberth353@gmail.com>
2023-12-11 10:06:23 +0700

    Add logout button, and check if customer/montir not logged in

---
+++ public/montir/js/checklogin.js
@@ -0,0 +1,21 @@
+const loginStatus = document.getElementById("loginStatus");
+if(localStorage.getItem("montirKey") === null) {
+	loginStatus.innerHTML += `
+        <dialog open>
+        <article>
+        <h3>You are not logged in!</h3>
+        <p>
+          Please log in first, to use montirku!
+        </p>
+        <footer>
+        <button onclick="window.location='../auth/login.html';">Log In</button>
+        </footer>
+        </article>
+        </dialog>
+    `;
+}
+
+function logout() {
+	localStorage.clear();
+	location.reload(); 
+}
